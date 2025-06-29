3cc96bfd78e7e68560b82d3918deaaa866678cac
Giu Eminente <giulemi210@gmail.com>
2025-06-29 20:10:30 +0200

    animations added, features section completed

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ const openCloseMenu = () => {
   menu.classList.toggle("open");
 };
 
+
+const feature1 = document.querySelector("#tab-1");
+const feature2 = document.querySelector("#tab-2");
+const feature3 = document.querySelector("#tab-3");
+const select1= document.querySelector("#sel-1");
+const select2= document.querySelector("#sel-2");
+const select3= document.querySelector("#sel-3");
+const selectFeature1 =() => {
+  select1.classList.add("selected");
+  select2.classList.remove("selected");
+  select3.classList.remove("selected");
+  feature1.classList.remove("closed");
+  feature2.classList.add("closed");
+  feature3.classList.add("closed");
+}
+const selectFeature2 =() => {
+  select1.classList.remove("selected");
+  select2.classList.add("selected");
+  select3.classList.remove("selected");
+  feature1.classList.add("closed");
+  feature2.classList.remove("closed");
+  feature3.classList.add("closed");
+}
+const selectFeature3 =() => {
+  select1.classList.remove("selected");
+  select2.classList.remove("selected");
+  select3.classList.add("selected");
+  feature1.classList.add("closed");
+  feature2.classList.add("closed");
+  feature3.classList.remove("closed");
+}
